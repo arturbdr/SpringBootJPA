00a6aee16b25eacc6e0ed0af0b4ff391d3ca10cd
arturbdr <cadGitHub123>
2016-12-17 16:36:25 -0200

    - updated rest url

--- src/main/java/br/com/aprendendo/jpa/HelloController.java
+++ src/main/java/br/com/aprendendo/jpa/HelloController.java
@@ -6,6 +6,7 @@ import java.util.Set;
 
 import javax.annotation.PostConstruct;
 
+import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,46 +20,43 @@ import br.com.aprendendo.jpa.domain.repository.CasaRepository;
 import br.com.aprendendo.jpa.domain.repository.PessoaRepository;
 
 @RestController
+@AllArgsConstructor
 public class HelloController {
-	
-	@Autowired
-	private CasaRepository casaRepository;
-	
-	@Autowired
-	private PessoaRepository pessoaRepository;
 
-	@PostConstruct
-	public void init() {
-		// popula a base de dados com valores iniciais
-		
-		Set<Casa> casasPessoa1 = new HashSet<Casa>();
-		Set<Casa> casasPessoa2 = new HashSet<Casa>();
-		Set<Casa> casasPessoa3 = new HashSet<Casa>();
-		
-		casasPessoa1.add(new Casa("Casa normal 1"));
-		casasPessoa2.add(new Casa("Casa 2a"));
-		casasPessoa2.add(new Casa("Casa 2b"));
-		casasPessoa3.add(new Casa("Casa pequena"));
-		
-		Pessoa pessoa1 = new Pessoa("Pessoa da casa normal", casasPessoa1);
-		Pessoa pessoa2 = new Pessoa("Pessoa das varias casas", casasPessoa2);
-		Pessoa pessoa3 = new Pessoa("Pessoa da casa menor", casasPessoa3);
-		
-		pessoaRepository.save(pessoa1);
-		pessoaRepository.save(pessoa2);
-		pessoaRepository.save(pessoa3);
-	}
-	
-	@RequestMapping("/getPessoas")
-	public List<Pessoa> getAllPessoas() {
-		return Lists.newArrayList(pessoaRepository.findAll());
-	}
-	
-	@RequestMapping("/get/pessoa/id/{id}")
-	public Pessoa getByPessoasByName(@PathVariable Long id) {
-		return pessoaRepository.findOne(id);
-	}
-	
-	
+    @Autowired
+    private final PessoaRepository pessoaRepository;
+
+    @PostConstruct
+    public void init() {
+        // popula a base de dados com valores iniciais
+
+        Set<Casa> casasPessoa1 = new HashSet<>();
+        Set<Casa> casasPessoa2 = new HashSet<>();
+        Set<Casa> casasPessoa3 = new HashSet<>();
+
+        casasPessoa1.add(new Casa("Casa normal 1"));
+        casasPessoa2.add(new Casa("Casa 2a"));
+        casasPessoa2.add(new Casa("Casa 2b"));
+        casasPessoa3.add(new Casa("Casa pequena"));
+
+        Pessoa pessoa1 = new Pessoa("Pessoa da casa normal", casasPessoa1);
+        Pessoa pessoa2 = new Pessoa("Pessoa das varias casas", casasPessoa2);
+        Pessoa pessoa3 = new Pessoa("Pessoa da casa menor", casasPessoa3);
+
+        pessoaRepository.save(pessoa1);
+        pessoaRepository.save(pessoa2);
+        pessoaRepository.save(pessoa3);
+    }
+
+    @RequestMapping("/pessoa/list")
+    public List<Pessoa> getAllPessoas() {
+        return Lists.newArrayList(pessoaRepository.findAll());
+    }
+
+    @RequestMapping("/pessoa/id/{id}")
+    public Pessoa getByPessoasByName(@PathVariable Long id) {
+        return pessoaRepository.findOne(id);
+    }
+
 
 }
